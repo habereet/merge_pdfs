f1fdbb0504f8bcfa9a63026149eda540d142accc
habereet <habereet@gmail.com>
2020-11-21 20:23:54 -0500

    flake8

--- mergePDF.py
+++ mergePDF.py
@@ -3,11 +3,11 @@ from PyPDF2 import PdfFileMerger
 
 loc = "basepdfs/"
 
-files = sorted(os.listdir(loc)) 
+files = sorted(os.listdir(loc))
 files = [file for file in files if file.endswith(".pdf")]
 
 merger = PdfFileMerger()
 for pdf in files:
-    merger.append(open(f'basepdfs/{pdf}','rb'))
+    merger.append(open(f'basepdfs/{pdf}', 'rb'))
 with open("result.pdf", "wb") as fout:
     merger.write(fout)
